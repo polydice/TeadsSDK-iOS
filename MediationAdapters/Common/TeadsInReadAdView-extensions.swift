4c0247af9ed322b84a8bfbf88fe363ff7255271c
kamikuo <kamijiawei@gmail.com>
2023-07-11 13:01:57 +0800

    custom size

--- MediationAdapters/Common/TeadsInReadAdView-extensions.swift
+++ MediationAdapters/Common/TeadsInReadAdView-extensions.swift
@@ -9,10 +9,22 @@ import TeadsSDK
 import UIKit
 
 public extension TeadsInReadAdView {
-    func updateHeight(with adRatio: TeadsAdRatio) {
-        if let width = superview?.frame.width.positive ?? frame.width.positive {
-            frame = CGRect(origin: CGPoint.zero, size: CGSize(width: width, height: adRatio.calculateHeight(for: width)))
-            layoutIfNeeded()
+    public func updateHeight() {
+        guard
+          window != nil,
+          let adRatio,
+          let width = superview?.frame.width.positive ?? frame.width.positive
+        else { return }
+        frame = CGRect(origin: .zero, size: sizeThatFits(width: width))
+        layoutIfNeeded()
+    }
+
+    public func sizeThatFits(width: CGFloat) -> CGSize {
+        guard let adRatio else { return .zero }
+        var size = CGSize(width: width, height: ceil(adRatio.calculateHeight(for: width)))
+        if size.height > 440 {
+            size = CGSize(width: size.width * (440 / size.height), height: 440)
         }
+        return size
     }
 }
